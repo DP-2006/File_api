5d86cbb2b162c69389b49128e74893e690300b5c
Danial <danialpourmehdi@gmail.com>
2026-09-14 12:12:37 +0330

    install all cdn

--- kiosk/settings.py
+++ kiosk/settings.py
@@ -333,8 +333,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'  # ✅ فقط این یک خط بماند
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_URL = '/static/'  # ✅ فقط این یک خط بماند
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
+
 
 # Media files (Uploaded files)
 MEDIA_URL = '/uploads/'
